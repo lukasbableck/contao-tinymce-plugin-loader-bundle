7f61a915f710e53b0532b1379fb77b7fc3d8b0b3
Lukas Bableck <lukas@bableck.dev>
2024-06-26 13:49:44 +0200

    register model in config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,7 +1,11 @@
 <?php
 
+use Lukasbableck\ContaoTinymcePluginLoaderBundle\Models\TinymcePluginModel;
+
 $GLOBALS['BE_MOD']['system'] = [
 	'tinymce_plugins' => [
 		'tables' => ['tl_tinymce_plugin'],
 	],
 ];
+
+$GLOBALS['TL_MODELS']['tl_tinymce_plugin'] = TinymcePluginModel::class;
