80e6f959f407996acdd5996e2ccc4ae614d341d5
Lukas Bableck <lukas@bableck.dev>
2024-06-26 14:04:32 +0200

    fix backend menu

--- contao/config/config.php
+++ contao/config/config.php
@@ -2,10 +2,8 @@
 
 use Lukasbableck\ContaoTinymcePluginLoaderBundle\Models\TinymcePluginModel;
 
-$GLOBALS['BE_MOD']['system'][] = [
-	'tinymce_plugins' => [
-		'tables' => ['tl_tinymce_plugin'],
-	],
+$GLOBALS['BE_MOD']['system']['tinymce_plugins'] = [
+	'tables' => ['tl_tinymce_plugin'],
 ];
 
 $GLOBALS['TL_MODELS']['tl_tinymce_plugin'] = TinymcePluginModel::class;
