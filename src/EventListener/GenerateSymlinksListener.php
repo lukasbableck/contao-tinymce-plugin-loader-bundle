c1adbc5d4edfe3f1d968f3395d8dffa532fd8c23
Lukas Bableck <lukas@bableck.dev>
2024-06-26 13:57:21 +0200

    initialize ContaoFramework and check if table exists in GenerateSymlinksListener

--- src/EventListener/GenerateSymlinksListener.php
+++ src/EventListener/GenerateSymlinksListener.php
@@ -4,23 +4,33 @@ namespace Lukasbableck\ContaoTinymcePluginLoaderBundle\EventListener;
 
 use Contao\CoreBundle\Event\ContaoCoreEvents;
 use Contao\CoreBundle\Event\GenerateSymlinksEvent;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\FilesModel;
 use Contao\System;
+use Doctrine\DBAL\Connection;
 use Lukasbableck\ContaoTinymcePluginLoaderBundle\Models\TinymcePluginModel;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 #[AsEventListener(ContaoCoreEvents::GENERATE_SYMLINKS)]
 class GenerateSymlinksListener {
+	public function __construct(private ContaoFramework $contaoFramework, private Connection $connection) {
+	}
+
 	public function __invoke(GenerateSymlinksEvent $event): void {
+		$this->contaoFramework->initialize();
+		$table = TinymcePluginModel::getTable();
+		if (!$this->connection->createSchemaManager()->tablesExist([$table])) {
+			return;
+		}
 		$linkPath = 'assets/tinymce4/js/plugins/';
 		$plugins = TinymcePluginModel::findAll();
 		foreach ($plugins as $plugin) {
 			$script = FilesModel::findByUuid($plugin->script)->path;
-			if($script === null) {
+			if ($script === null) {
 				continue;
 			}
 			$projectDir = System::getContainer()->getParameter('kernel.project_dir');
-			if(file_exists($projectDir.'/'.$linkPath.$plugin->name.'/'.$plugin->name.'.js')) {
+			if (file_exists($projectDir.'/'.$linkPath.$plugin->name.'/'.$plugin->name.'.js')) {
 				continue;
 			}
 			$event->addSymlink($script, $linkPath.$plugin->name.'/'.$plugin->name.'.js');
